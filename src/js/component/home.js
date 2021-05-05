8b100c80067d4fd13c617046dfd4d0fad6d65406
pbullor <pablobullor@gmail.com>
2021-05-05 10:37:04 +0000

    subo ejemplos

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	//fetch
+	let [pokeFetch, setPokeFetch] = useState([]);
+	let [pokeAsync, setPokeAsync] = useState([]);
+
+	fetch("https://pokeapi.co/api/v2/pokemon")
+		.then(res => res.json())
+		.then(data => {
+			setPokeFetch(data.results);
+		})
+		.catch(error => console.log(error));
+
+	//async await
+
+	const obtenerPokemones = async () => {
+		try {
+			const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+			const data = await res.json();
+			setPokeAsync(data.results);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	obtenerPokemones();
+
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!</h1>
